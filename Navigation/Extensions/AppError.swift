6f4ba2e260133979986efe2e83f0eae3ca9d2d57
Roman Leshin <vorzee@gmail.com>
2024-10-11 23:56:20 +0500

    Task 1 complete

---
+++ Navigation/Extensions/AppError.swift
@@ -0,0 +1,13 @@
+//
+//  AppError.swift
+//  Navigation
+//
+//  Created by Роман Лешин on 11.10.2024.
+//
+
+enum AppError: Error {
+    case networkUnavailable
+    case unauthorized
+    case dataNotFound
+    case unknownError
+}

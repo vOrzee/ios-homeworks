b53dc1a4bfc837dec96d1850bf30eeb1336fb2a9
Roman Leshin <vorzee@gmail.com>
2024-11-23 00:24:29 +0500

    Add extension UIColor

---
+++ Navigation/Extensions/ThemeUIColorCreator.swift
@@ -0,0 +1,18 @@
+//
+//  ThemeUIColorCreator.swift
+//  Navigation
+//
+//  Created by Роман Лешин on 23.11.2024.
+//
+import UIKit
+
+extension UIColor {
+    static func createColor(lightMode: UIColor, darkMode: UIColor) -> UIColor {
+        guard #available(iOS 13.0, *) else {
+            return lightMode
+        }
+        return UIColor { (traitCollection) -> UIColor in
+            return traitCollection.userInterfaceStyle == .light ? lightMode : darkMode
+        }
+    }
+}
